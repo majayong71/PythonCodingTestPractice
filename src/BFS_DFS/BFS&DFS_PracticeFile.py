c81ce52eb4cd2a79bcb2495ec09ae1ab778878f7
majayong71 <wotkd2647@naver.com>
2026-03-20 16:35:15 +0900

    algorithm practice

--- src/BFS_DFS/BFS&DFS_PracticeFile.py
+++ src/BFS_DFS/BFS&DFS_PracticeFile.py
@@ -18,3 +18,8 @@ def solution():
     n, m, v = map(int, input().split())
 
     graph= [[] for _ in range(n + 1)]
+
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
