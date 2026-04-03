8855d7f095e57f9615182142cc8dc49c13c97acf
majayong71 <wotkd2647@naver.com>
2026-04-03 17:29:45 +0900

    algo practice commit

--- src/BFS_DFS/BFS&DFS_PracticeFile.py
+++ src/BFS_DFS/BFS&DFS_PracticeFile.py
@@ -14,16 +14,3 @@ from collections import deque
 
 # =============7. 출력 ============
 
-def solution():
-    n, m, v = map(int, input().split())
-
-    graph= [[] for _ in range(n + 1)]
-
-    for _ in range(m):
-        a, b = map(int, input().split())
-        graph[a].append(b)
-        graph[b].append(a)
-
-    for i in range(1, n+1):
-        graph[i].sort()
-    
