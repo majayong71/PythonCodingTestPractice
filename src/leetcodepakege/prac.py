fbcf199d889b4b63be1e566a34a729dab908854d
majayong71 <wotkd2647@naver.com>
2026-05-14 16:45:12 +0900

    leetcode twoSum.py

---
+++ src/leetcodepakege/prac.py
@@ -0,0 +1,18 @@
+# TWOSUM 문제 ,
+# 배열 안에 있는 값 을 더해
+# 타겟의 값이 나오게 해라
+# ex) [1,3,5,7,9] , target:16
+# 3, 4
+
+class Solution:
+    def twosum(self, nums,target):
+        indexed =[]
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()                      # Java: Solution s = new Solution()
+    result = s.twosum([2,3,5,7,11,13,17,19,23,29,31,37], 68)  # Java: s.twoSum(new int[]{2,3,5,7,11,13,17,19,23,29,31,37}, 68)
+    print(result)
+
--- src/leetcodepakege/twoSum.py
+++ src/leetcodepakege/twoSum.py
@@ -1,40 +1,46 @@
 from typing import List
 
 class Solution:
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+        # 값과 원본 인덱스를 쌍으로 묶어 저장할 빈 리스트
         indexed = []
+
+        # nums를 순회하며 (값, 원본인덱스) 쌍을 indexed에 추가
         for i in range(len(nums)):
             indexed.append((nums[i], i))
 
+        # 투포인터를 쓰기 위해 값 기준으로 오름차순 정렬
         indexed.sort()
 
-        left  = 0
+        # 제일 작은 값을 가리키는 왼쪽 포인터
+        left = 0
+        # 제일 큰 값을 가리키는 오른쪽 포인터
         right = len(indexed) - 1
 
+        # 두 포인터가 만나기 전까지 반복
         while left < right:
+
+            # 현재 왼쪽 값 + 오른쪽 값의 합계
             total = indexed[left][0] + indexed[right][0]
 
             if total == target:
+                # 정답 찾음 → 두 값의 원본 인덱스 반환
                 return [indexed[left][1], indexed[right][1]]
             elif total < target:
+                # 합이 target보다 작으면 왼쪽 포인터를 오른쪽으로 이동 (값 키우기)
                 left += 1
             else:
+                # 합이 target보다 크면 오른쪽 포인터를 왼쪽으로 이동 (값 줄이기)
                 right -= 1
 
-    def twoSum2(self, nums: List[int], target: int) -> List[int]:
-
-        indexed = []
-        for i in range(len(nums)):
-            indexed.append((nums[i], i))
-
-        indexed.sort()
-
-        left = 0
-        right = len(indexed) - 1
-
-
 if __name__ == '__main__':
-    s = Solution()                      # Java: Solution s = new Solution()
-    result = s.twoSum([2,3,5,7,11,13,17,19,23,29,31,37], 68)  # Java: s.twoSum(new int[]{2,3,5,7,11,13,17,19,23,29,31,37}, 68)
-    print(result)                       # [ , ]
+    # Solution 인스턴스 생성 (Java: Solution s = new Solution())
+    s = Solution()
+
+    # twosum 호출 후 결과 저장
+    result = s.twosum([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37], 68)
+
+    # 결과 출력
+    print(result)                    # [ , ]
